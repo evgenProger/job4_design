b115f20268f8d563179ee0d7ca9d949a9aa4282b
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2020-10-15 09:22:00 +0500

    Iterator

--- chapter_005/src/test/java/ru/job4j/it/BackwardArrayItTest.java
+++ chapter_005/src/test/java/ru/job4j/it/BackwardArrayItTest.java
@@ -2,6 +2,8 @@ package ru.job4j.it;
 
 import org.junit.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.hamcrest.core.Is.*;
 import static org.junit.Assert.*;
 
@@ -9,11 +11,27 @@ public class BackwardArrayItTest {
     @Test
     public void whenReadSequence() {
         BackwardArrayIt it = new BackwardArrayIt(
-                new int[] {1, 2, 3}
+                new int[]{1, 2, 3}
         );
         assertThat(it.next(), is(3));
         assertThat(it.next(), is(2));
         assertThat(it.next(), is(1));
     }
+
+    @Test
+    public void whenMultiCallhasNextThenTrue() {
+        BackwardArrayIt it = new BackwardArrayIt(new int[]{1, 2, 3});
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whennextFromEmpty() {
+        BackwardArrayIt it = new BackwardArrayIt(new int[] {});
+        it.next();
+    }
+
+
+
 
 }
