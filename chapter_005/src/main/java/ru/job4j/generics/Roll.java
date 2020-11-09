c6332e98daa06af9d49b152e020fc58ad0acf382
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2020-11-09 20:05:34 +0500

    Containers

---
+++ chapter_005/src/main/java/ru/job4j/generics/Roll.java
@@ -0,0 +1,8 @@
+package ru.job4j.generics;
+
+public class Roll extends Base {
+
+    protected Roll(String id) {
+        super(id);
+    }
+}
---
+++ chapter_005/src/main/java/ru/job4j/generics/RollStore.java
@@ -0,0 +1,25 @@
+package ru.job4j.generics;
+
+public class RollStore implements Store<Roll> {
+    private final Store<Roll> store = new MemStore<>();
+
+    @Override
+    public void add(Roll model) {
+        store.add(model);
+    }
+
+    @Override
+    public boolean replace(String id, Roll model) {
+        return store.replace(id, model);
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return store.delete(id);
+    }
+
+    @Override
+    public Roll findById(String id) {
+        return store.findById(id);
+    }
+}
