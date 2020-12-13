c18bce04688427cea13584ffb023901a59d86be4
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2020-12-13 13:59:14 +0500

    SimpleSet

--- chapter_005/src/test/java/ru/job4j/collection/SimpleSetTest.java
+++ chapter_005/src/test/java/ru/job4j/collection/SimpleSetTest.java
@@ -10,17 +10,24 @@ import static org.junit.Assert.*;
 
 public class SimpleSetTest {
     @Test
-    public void whenAddThenGet() {
+    public void whenAdd() {
         SimpleSet<String> array = new SimpleSet<>();
         assertTrue(array.add("first000"));
     }
 
     @Test
-    public void whenAddDublicate() {
+    public void whenAddDubllicate() {
         SimpleSet<String> array = new SimpleSet<>();
         array.add("first");
         array.add("two");
         boolean res =  array.add("first");
         assertThat(res, is(false));
     }
+
+    @Test
+    public void whenAddNullThenTrue() {
+        SimpleSet<String> array = new SimpleSet<>();
+        assertTrue(array.add(null));
+    }
+
 }
