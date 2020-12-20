36bc392e7c52c53ad81aff10f881db431e60e36b
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2020-12-20 18:23:43 +0500

    Override HashCode

--- chapter_005/src/main/java/ru/job4j/collection/User1.java
+++ chapter_005/src/main/java/ru/job4j/collection/User1.java
@@ -5,15 +5,24 @@ import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
 
-public class User {
+public class User1 {
     String name;
     int children;
     Calendar birthday;
 
-    public User(String name, int children, Calendar birthday) {
+    public User1(String name, int children, Calendar birthday) {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        result = 37 * children;
+        result = result * name.hashCode();
+        result = result * birthday.hashCode();
+        return result;
     }
 
     public static class Car {
@@ -21,18 +30,21 @@ public class User {
         String name;
 
         public Car(String color, String name) {
+            super();
             this.color = color;
             this.name = name;
         }
     }
+
+
     public static void main(String[] args) {
         Calendar c1 = new GregorianCalendar(1980, 10, 15);
         Calendar c2 = new GregorianCalendar(1980, 10, 15);
-        User user1  = new User("Evgeny", 2, c1);
-        User user2 = new User("Evgeny", 2, c2);
+        User1 user1  = new User1("Evgeny", 2, c1);
+        User1 user2 = new User1("Evgeny", 2, c2);
         Car car1 = new Car("red", "bmw");
         Car car2 = new Car("black", "mersedes");
-        Map<User, Car> users = new HashMap<>();
+        Map<User1, Car> users = new HashMap<>();
         users.put(user1, car1);
         users.put(user2, car2);
         System.out.println(users);
