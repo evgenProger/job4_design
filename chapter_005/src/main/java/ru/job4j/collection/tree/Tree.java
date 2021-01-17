d0b5740ed1a3b79780d6ce4344365fa97124f7fb
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-01-17 18:05:16 +0500

    Simple Tree

--- chapter_005/src/main/java/ru/job4j/collection/tree/Tree.java
+++ chapter_005/src/main/java/ru/job4j/collection/tree/Tree.java
@@ -7,7 +7,6 @@ import java.util.function.Predicate;
 
 public class Tree<E> implements SimpleTree<E> {
     private final Node<E> root;
-    private Predicate<E> predicate;
 
     public Tree(final E root ) {
         this.root = new Node<E>(root);
@@ -24,47 +23,28 @@ public class Tree<E> implements SimpleTree<E> {
         return rs;
     }
 
-    public boolean findByPredicate(Predicate<E> predicate, E value) {
-        boolean rs = false;
+    public Optional<Node<E>> findByPredicate(Predicate<Node<E>> predicate) {
+        Optional<Node<E>> rs1 = Optional.empty();
         Node<E> current;
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(root);
         while (!data.isEmpty()) {
             current = data.poll();
-            if (predicate.test(value))) {
-                rs = true;
+            if (predicate.test(current)) {
+                rs1 = Optional.of(current);
                 break;
             }
             data.addAll(current.children);
         }
-        return rs;
+        return rs1;
     }
 
     public boolean isBinary() {
-        boolean rs = true;
-        Predicate<Integer> pre = p -> p > 2;
-        Node<E> current = root;
-        Queue<Node<E>> data = new LinkedList<>();
-        if (current.children.size() > 2) {
-            return false;
-        }
-        data.addAll(current.children);
-        /* while (!data.isEmpty()) {
-            current = data.poll();
-            if (current.children.size() > 2) {
-                rs = false;
-                break;
-            }
-            data.addAll(current.children);
-        }
-       */ if (findByPredicate(pre, current.children.size()) {
-           rs = false;
-        }
-        return rs;
+       return findByPredicate(node -> node.children.size() > 2).isEmpty();
     }
     @Override
     public Optional<Node<E>> findBy(E value) {
-        Optional<Node<E>> rs1 = Optional.empty();
+       /* Optional<Node<E>> rs1 = Optional.empty();
         Node<E> current;
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
@@ -75,7 +55,10 @@ public class Tree<E> implements SimpleTree<E> {
                 break;
             }
             data.addAll(current.children);
+
+
         }
-        return rs1;
+        */
+        return findByPredicate(node -> node.value.equals(value));
     }
 }
