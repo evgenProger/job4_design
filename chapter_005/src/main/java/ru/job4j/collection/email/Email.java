21c04bb09383a6d8401995f0860fae6ba8b99124
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-01-24 16:23:07 +0500

    E-mail

--- chapter_005/src/main/java/ru/job4j/collection/email/Email.java
+++ chapter_005/src/main/java/ru/job4j/collection/email/Email.java
@@ -6,7 +6,7 @@ public class Email {
 
 
 
-    public HashMap<String, List<String>> unionEmails(HashMap<String, List<String>> email_accounts) {
+    public HashMap<String, Set<String>> unionEmails(HashMap<String, List<String>> email_accounts) {
         Map<String, Set<String>> res = new HashMap<>();
         List<String> users = new ArrayList<>();
         users.addAll(email_accounts.keySet());
@@ -17,82 +17,34 @@ public class Email {
         List<String> total = new ArrayList<>();
         Boolean flag = false;
         int i = 0;
-        int j= 1;
+        int j = 1;
         int k = 0;
-        user = users.get(0);
-        res.put(user, (Set<String>) email_accounts.get(users.get(i)));
+
         while (i < users.size()) {
+            user = users.get(i);
+            res.put(user, (Set<String>) email_accounts.get(users.get(i)));
             previous.addAll(res.get(user));
             next.addAll(email_accounts.get(users.get(j)));
             for (int m = 0; m < previous.size(); m++) {
                 for (int n = 0; n < next.size(); n++) {
                     if (previous.get(m).equals(next.get(n))) {
-                        res.put(user, (Set<String>) next);
+                        res.get(user).addAll(next);
+                        i++;
+                        flag = true;
+
+                        break;
                     }
+                    else {
+
+                    }
+                }
+                if (flag) {
+                    break;
                 }
             }
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*  while (i < users.size()) {
-            emails.addAll(email_accounts.get(users.get(i)));
-            while (p < 2) {
-                for (int j = i + 1; j < users.size(); j++) {
-                    next.addAll(email_accounts.get(users.get(j)));
-                    for (int m = 0; m < next.size(); m++) {
-                        if (emails.contains(next.get(m))) {
-                            emails.addAll(next);
-                            user = users.get(i);
-                            flag = true;
-                            break;
-                        }
-
-                    }
-                    next.removeAll(next);
-                }
-                p++;
-            }
-            p = 0;
-            if (flag) {
-                result.addAll(emails);
-                res.put(user, result);
-
-            }
-            flag = false;
-            i++;
-            emails.removeAll(emails);
-        }
-       */
-        return (HashMap<String, List<String>>) res;
+        return (HashMap<String, Set<String>>) res;
     }
-
 
 }
 
