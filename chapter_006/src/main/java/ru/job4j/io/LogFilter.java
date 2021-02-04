138d10701323f97e70671b8db8e6e7e4dfa34dde
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-02-04 16:45:39 +0500

    Filter of text

--- chapter_006/src/main/java/ru/job4j/io/LogFilter.java
+++ chapter_006/src/main/java/ru/job4j/io/LogFilter.java
@@ -1,7 +1,6 @@
 package ru.job4j.io;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -18,7 +17,6 @@ public class LogFilter {
                 }
             }
         }
-
         return res;
     }
 
