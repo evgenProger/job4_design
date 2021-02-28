32291d02150c2d67db1e1fd49a9d96c128eb279f
evgeny <ivanov.evgen.v@gmail.com>
2021-02-28 13:50:27 +0400

    Substitution pattern

--- chapter_006/src/main/java/ru/job4j/io/UsageLog4j.java
+++ chapter_006/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -4,12 +4,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
-
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte b = 24;
+        short s = 30254;
+        int i = 1256321;
+        long l = 1235487432;
+        float f = 35.254f;
+        double d =  896.547d;
+        char ch = 'H';
+        boolean bool = false;
+        LOG.debug("This is byte type: {}", b);
+        LOG.debug("This is short type: {}", s);
+        LOG.debug("This is int type: {}", i);
+        LOG.debug("This is long type: {}", l);
+        LOG.debug("This is float type: {}", f);
+        LOG.debug("This is double type: {}", d);
+        LOG.debug("This is char type: {}", ch);
+        LOG.debug("This is boolean type: {}", bool);
+
     }
 }
