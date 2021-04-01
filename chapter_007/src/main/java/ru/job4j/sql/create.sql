90a7982d0f86244afc97378da29ff0a53b582866
evgeny <ivanov.evgen.v@gmail.com>
2021-04-01 11:57:54 +0500

    create DB

--- chapter_007/src/main/java/ru/job4j/sql/create.sql
+++ chapter_007/src/main/java/ru/job4j/sql/create.sql
@@ -6,7 +6,7 @@ CREATE TABLE "user" (
 CREATE TABLE "role" (
   "id"  SERIAL  PRIMARY KEY,
   "name" varchar(2000),
-  "user_id" int
+  "user_id" int references "user"(id)
 );
 
 CREATE TABLE "rules" (
@@ -16,60 +16,37 @@ CREATE TABLE "rules" (
 
 CREATE TABLE "role_rules" (
   "id"  SERIAL PRIMARY KEY,
-  "role_id" int ,
-  "rules_id" int
+  "role_id" int references role(id),
+  "rules_id" int references rules(id)
 );
 
 CREATE TABLE "item" (
   "id"  SERIAL PRIMARY KEY,
   "name" varchar(200),
-  "user_id" int
-);
-
-CREATE TABLE "item_user" (
-  "id"  SERIAL PRIMARY KEY,
-  "item_id" int,
-  "user_id" int
+  "user_id" int references "user"(id),
+  "category_id" int references "category"(id),
+  "state_id" int references "state"(id)
 );
 
 CREATE TABLE "comments" (
   "id" SERIAL PRIMARY KEY,
   "comment" text,
-  "item_id" int
+  "item_id" int references item(id)
 );
 
 CREATE TABLE "attachs" (
   "id" SERIAL PRIMARY KEY,
   name "varchar" (200),
-  "item_id" int
+  "item_id" int references item(id)
 );
 
 CREATE TABLE "category" (
   "id"  SERIAL PRIMARY KEY,
-  "name" varchar(200),
-  "item_id" int
+  "name" varchar(200)
 );
 
 CREATE TABLE "state" (
   "id" SERIAL PRIMARY KEY,
-  "st" varchar(200),
-  "item_id" int
+  "status" varchar(200)
 );
-
-ALTER TABLE "role" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id") ;
-
-ALTER TABLE "role_rules" ADD FOREIGN KEY ("role_id") REFERENCES "role" ("id");
-
-ALTER TABLE "role_rules" ADD FOREIGN KEY ("rules_id") REFERENCES "rules" ("id");
-
-ALTER TABLE "item" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
-
-ALTER TABLE "comments" ADD FOREIGN KEY ("item_id") REFERENCES "item" ("id");
-
-ALTER TABLE "attachs" ADD FOREIGN KEY ("item_id") REFERENCES "item" ("id");
-
-ALTER TABLE "category" ADD FOREIGN KEY ("item_id") REFERENCES "item" ("id");
-
-ALTER TABLE "state" ADD FOREIGN KEY ("item_id") REFERENCES "item" ("id");
-
 
--- chapter_007/src/main/java/ru/job4j/sql/insert.sql
+++ chapter_007/src/main/java/ru/job4j/sql/insert.sql
@@ -1,26 +1,56 @@
 insert into "user" (name) values ('user1');
 insert into "user" (name) values ('user2');
 insert into "user" (name) values ('user3');
+insert into "user"(name) values ('user4');
 insert into "role" (name, user_id) values ('operator', 1);
 insert into "role" (name, user_id) values ('administrator', 2);
 insert into "role"(name, user_id) VALUES ('director', 3);
+insert into role(name, user_id) VALUES ('storeman', 4);
 insert into "rules"(name) values ('add');
 insert into "rules"(name) values ('delete');
 insert into "rules"(name) values ('create');
 insert into "rules"(name) values ('comment');
-insert into "role_rules"(role_id, rules_id) VALUES (1, 1);
+insert into "rules"(name) values ('read');
+insert into "role_rules"(role_id, rules_id) VALUES (1, 4);
+insert into role_rules(role_id, rules_id) VALUES (1, 4);
 insert into "role_rules"(role_id, rules_id) VALUES (3, 1);
 insert into "role_rules"(role_id, rules_id) VALUES (3, 2);
 insert into "role_rules"(role_id, rules_id) VALUES (3, 3);
-insert into "role_rules"(role_id, rules_id) VALUES (4, 1);
-insert into "role_rules"(role_id, rules_id) VALUES (4, 2);
-insert into "role_rules"(role_id, rules_id) VALUES (4, 3);
+insert into "role_rules"(role_id, rules_id) VALUES (3, 4);
 insert into "role_rules"(role_id, rules_id) VALUES (4, 4);
-insert into "role_rules"(role_id, rules_id) VALUES (1, 3);
-insert into "item"(name, user_id) VALUES ('computers', 1);
-insert into "item"(name, user_id) VALUES ('tv', 2);
-insert into "item"(name, user_id) VALUES ('washer', 1);
-insert into "item"(name, user_id) VALUES ('microwave', 3);
+insert into "role_rules"(role_id, rules_id) VALUES (2, 1);
+insert into "role_rules"(role_id, rules_id) VALUES (2, 4);
+insert into "role_rules"(role_id, rules_id) VALUES (2, 3);
+insert into "category"(name) values ('household appliances');
+insert into "category"(name) values ('toys');
+insert into "category"(name) values ('sport');
+insert into "state"(status) values ('accepted');
+insert into "state"(status) values ('in processing');
+insert into "state"(status) values ('deleted');
+insert into "item"(name, user_id, category_id, state_id) VALUES ('item_№1', 1, 1, 1);
+insert into "item"(name, user_id, category_id, state_id) VALUES ('item_N2', 1, 2, 2);
+insert into "item"(name, user_id, category_id, state_id) VALUES ('item_N3', 2, 2, 3);
+insert into "item"(name, user_id, category_id, state_id) VALUES ('item_N4', 3, 3, 2);
+insert into "item"(name, user_id, category_id, state_id) VALUES ('item_N4', 3, 3, 3);
+insert into "item"(name, user_id, category_id, state_id) VALUES ('item_N4', 1, 2, 3);
+insert into "attachs"(name, item_id) VALUES ('blank', 1);
+insert into "attachs"(name, item_id) VALUES ('picture of computer', 2);
+insert into attachs(name, item_id) VALUES ('description', 2);
+insert into "attachs"(name, item_id) VALUES ('picture of washer', 3);
+insert into "attachs"(name, item_id) VALUES ('report', 4);
+insert into "attachs"(name, item_id) VALUES ('answer', 4);
+insert into comments(comment, item_id) VALUES ('thank you', 2);
+insert into comments(comment, item_id) VALUES ('I am satisfied' , 2);
+insert into comments(comment, item_id) VALUES ('everything is bad' , 1);
+insert into comments(comment, item_id) VALUES ('sorry for delay' , 3);
+
+
+
+
+
+
+
+
 
 
 
