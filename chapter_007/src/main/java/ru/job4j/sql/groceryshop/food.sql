0b051ca9da81b663b3254e5a092bab3e57f39103
evgeny <ivanov.evgen.v@gmail.com>
2021-04-07 18:12:31 +0500

    FoodBase Filter

--- chapter_007/src/main/java/ru/job4j/sql/groceryshop/food.sql
+++ chapter_007/src/main/java/ru/job4j/sql/groceryshop/food.sql
@@ -33,22 +33,33 @@ insert into food(name, expired_date, price, type_id) VALUES ('cake', '2021-06-14
 insert into food(name, expired_date, price, type_id) VALUES ('pie', '2021-06-03', 25, 3);
 insert into food(name, expired_date, price, type_id) VALUES ('ring roll', '2021-07-28', 59, 3);
 
-select name from food where type_id = 1;
+select f.name as наименование, tF.name as категория from food as f
+    join typeFood tF on tF.id = f.type_id
+    where tF.name = 'dairy';
 
 select name from food where name like '%cream%';
 
-select name from food where  expired_date > current_date + 30;
+select name from food where date_part('month', expired_date) = date_part('month', current_date + interval '1 month');
 
 select name from food where price = (select max(price) from food);
 
-select count(name) from food
-group by type_id;
+select tF.name, count(f.name)  from food as f join typeFood tF on tF.id = f.type_id
+group by tF.name;
 
 select tf.name as категория, count(f.name) as количество  from typeFood as tf
 join food f on tf.id = f.type_id
 group by tf.id;
 
-select name from food where type_id in (1, 2);
+select f.name from food as f join typeFood tF on tF.id = f.type_id
+where tF.name = 'dairy' or tF.name = 'bakery products';
+
+select tF.name from typeFood as tF join food f on tF.id = f.type_id
+group by tF.name
+having count(f.name) < 5;
+
+
+
+
 
 
 
