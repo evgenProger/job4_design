ca4c9fd099a73db68cc9c52de6f8a083484b6ac4
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-04-08 15:46:15 +0500

    Demo left join, right join, gull join and cross join

--- chapter_007/src/main/java/ru/job4j/sql/office/office.sql
+++ chapter_007/src/main/java/ru/job4j/sql/office/office.sql
@@ -47,3 +47,24 @@ insert into emp(name, dept_id) values('JAMES', 4);
 
 insert into emp(name, dept_id) values('MILLER', 2);
 
+insert into emp(name, dept_id) values ('Lenny', null);
+
+insert into emp(dept_id) VALUES (4);
+
+select e.name as emplayer, d.name as department from emp as e
+left join dept d on e.dept_id = d.id;
+
+select d.name as department, e.name as emplayers from dept as d
+    right join emp as e on d.id = e.dept_id;
+
+select e.name as emplayer, d.name as department from emp as e
+full join dept d on e.dept_id = d.id;
+
+select * from emp cross join dept;
+
+select d.name from emp e
+left join dept d on e.dept_id = d.id
+where e.name is null
+
+
+
--- simulationCdLinux/src/main/java/Shell.java
+++ simulationCdLinux/src/main/java/Shell.java
@@ -4,6 +4,11 @@ public class Shell {
     private LinkedList<String> res = new LinkedList<>();
 
     public void cd(String path) {
+        StringBuilder sb = new StringBuilder();
+        if (path.equals("/")) {
+            res.addFirst(path);
+            return;
+        }
         String[] str;
         str = path.split("/");
         for (int i = 0; i < str.length; i++) {
@@ -12,29 +17,64 @@ public class Shell {
             }
             if (!str[i].equals("..")) {
                 res.addLast(str[i]);
-            } else {
-                res.pollLast();
             }
+            else {
+                res.pollFirst();
+            }
+
         }
     }
+
+
+
+
+       /* StringBuilder str = new StringBuilder();
+        if (path.equals("/")) {
+            res.addFirst(path);
+            return;
+        }
+        if (!path.endsWith("..")) {
+            str.append("/").append(path);
+            res.addLast(str.toString());
+        }
+        if (path.endsWith("..") && res.isEmpty()) {
+            str = new StringBuilder(path.substring(0, path.lastIndexOf("/")));
+            str = new StringBuilder(str.substring(0, str.lastIndexOf("/") + 1));
+            res.addFirst(str.toString());
+            return;
+
+        }
+        if (path.endsWith("..") && !res.isEmpty() && res.size() <= 2) {
+            str = new StringBuilder();
+            while (!res.isEmpty()) {
+                str.append(res.pollFirst());
+                str.append(path);
+            }
+            str = new StringBuilder(str.substring(0, str.lastIndexOf("/") + 1));
+            res.addFirst(str.toString());
+        }
+        else if (path.endsWith("..") && !res.isEmpty() && res.size() > 2){
+            int count = 0;
+            while (count < 2) {
+                str.append(res.pollFirst());
+                count++;
+            }
+            while (!res.isEmpty()) {
+                res.poll();
+            }
+            res.addFirst(str.toString());
+        }
+    }
+        */
+
     public String pwd() {
         StringBuilder sb = new StringBuilder();
-        if (res.isEmpty()) {
-            return "/";
-        }
         while (!res.isEmpty()) {
-            sb.append("/").append(res.pollFirst());
+            sb.append(res.pollFirst());
         }
         return sb.toString();
     }
 }
-
-
-
-
-
-
-
 
 
 
