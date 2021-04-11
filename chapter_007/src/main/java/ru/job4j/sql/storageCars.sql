0c5b28163dab11b4d5dcbb560acd4859e5366891
evgeny <ivanov.evgen.v@gmail.com>
2021-04-11 12:57:35 +0500

    Storage cars

--- chapter_007/src/main/java/ru/job4j/sql/storageCars.sql
+++ chapter_007/src/main/java/ru/job4j/sql/storageCars.sql
@@ -48,12 +48,21 @@ insert into car(name, carBody_id, engine_id, transmission_id) VALUES ('mercedes'
 insert into car(name, carBody_id, engine_id, transmission_id) VALUES ('volvo', 5, 4, 4);
 insert into car(name, carBody_id, engine_id, transmission_id) VALUES ('volkswagen', 4, 5, 2);
 insert into car(name, carBody_id, engine_id, transmission_id) VALUES ('mitsubishi', 3, 3, 3);
+insert into car(name, carBody_id, engine_id, transmission_id) VALUES (null , 1, 4, 2);
 
 select c.name as car, b.name as carBody, e.type as engine, t.type as transmission
 from car as c join carBody as b on c.carBody_id = b.id
 join engine as e on c.engine_id = e.id
-join transmission t on c.transmission_id = t.id
+join transmission t on c.transmission_id = t.id;
 
+select cB.name as carbody from car as c
+join carBody cB on c.carBody_id = cB.id where c.name is null;
+
+select e.type as engine from car as c
+join engine e on c.engine_id = e.id where c.name is null;
+
+select t.type as transmission from car as c
+join transmission t on c.transmission_id = t.id  where c.name is null;
 
 
 
