998d5fd660fbf8c395a35c0e816e623c6d84f86a
evgeny <ivanov.evgen.v@gmail.com>
2021-04-22 22:16:29 +0500

    Spammers database load

---
+++ chapter_007/src/main/java/ru/job4j/sql/jdbc/spammers/ImportDB.java
@@ -0,0 +1,65 @@
+package ru.job4j.sql.jdbc.spammers;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class ImportDB {
+    private Properties cfg;
+    private String dump;
+
+    public ImportDB(Properties cfg, String dump) {
+        this.cfg = cfg;
+        this.dump = dump;
+    }
+    public List<User> load() {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
+            rd.lines().map(s -> new User(s.split(";")[0], s.split(";")[1])).forEach(users::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return  users;
+    }
+
+    public void save(List<User> users) throws ClassNotFoundException, SQLException {
+        Class.forName(cfg.getProperty("jdbc.driver"));
+        try (Connection cnt = DriverManager.getConnection(
+                cfg.getProperty("jdbc.url"),
+                cfg.getProperty("jdbc.username"),
+                cfg.getProperty("jdbc.password")
+        )) {
+            for (User user : users) {
+                try (PreparedStatement ps = cnt.prepareStatement("insert into  spammers (name, email) values (?, ?)")) {
+                    ps.setString(1, user.name);
+                    ps.setString(2, user.email);
+                    ps.execute();
+                }
+            }
+        }
+    }
+
+    private static class User {
+        String name;
+        String email;
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+        Properties cfg = new Properties();
+        try (FileInputStream in = new FileInputStream("chapter_007/src/main/resources/spammers.properties")) {
+            cfg.load(in);
+        }
+        ImportDB importDB = new ImportDB(cfg, "chapter_007/src/main/resources/dump.txt");
+        importDB.save(importDB.load());
+    }
+}
---
+++ chapter_007/src/main/java/ru/job4j/sql/jdbc/spammers/create.sql
@@ -0,0 +1,6 @@
+create table spammers
+(
+    id serial primary key,
+    name varchar(50),
+    email varchar(50)
+)
