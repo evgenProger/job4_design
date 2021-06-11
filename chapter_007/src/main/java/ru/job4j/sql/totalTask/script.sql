6221f4c55fdc6edca1b0539752d8d934d4aea6da
evgeny <ivanov.evgen.v@gmail.com>
2021-06-11 23:07:41 +0500

    Total task from SQL part

--- chapter_007/src/main/java/ru/job4j/sql/totalTask/script.sql
+++ chapter_007/src/main/java/ru/job4j/sql/totalTask/script.sql
@@ -1,4 +1,5 @@
-select m.name, count(*) as quality from meet as m left join user_meet um on m.id = um.meet_id
+select m.name, count(*) as quality from meet as m
+left join user_meet um on m.id = um.meet_id
 join user_meet_status ums on um.id = ums.id_user_meet
 where id_status = 1
 group by m.name;
