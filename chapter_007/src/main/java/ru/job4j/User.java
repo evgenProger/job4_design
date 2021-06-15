f00063b301929d025f7cf4440135f54672c56e30
evgeny <ivanov.evgen.v@gmail.com>
2021-06-15 22:35:13 +0500

    Size of variable

---
+++ chapter_007/src/main/java/ru/job4j/User.java
@@ -0,0 +1,44 @@
+package ru.job4j;
+
+import com.carrotsearch.sizeof.RamUsageEstimator;
+
+import static com.carrotsearch.sizeof.RamUsageEstimator.sizeOf;
+
+public class User {
+    private int age;
+    private String name;
+
+    public User(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected void finalize() {
+        System.out.printf("Removed %d, %s%n", age, name);
+    }
+
+    public static void main(String[] args) {
+        User userOne = new User(25, "Evgeny");
+        User userTwo = new User(35, "Kate");
+        System.out.println("Hello");
+        // System.out.println(RamUsageEstimator.sizeOf(userOne));
+    }
+}
+
