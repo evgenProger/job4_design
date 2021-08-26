fdffb8734f9cb7d3ce0a00b4c2e50c3e70939ee0
evgeny <ivanov.evgen.v@gmail.com>
2021-08-26 15:14:59 +0500

    Car parking, TDD

--- chapter_009/src/test/java/ru/job4j/solid/lsp/carparking/ParkingTest.java
+++ chapter_009/src/test/java/ru/job4j/solid/lsp/carparking/ParkingTest.java
@@ -6,66 +6,74 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ParkingTest {
-   private Car lightCar;
-   private Car truck;
-
-   @Before
-   public void initialCars() {
-      lightCar = new LightCar();
-      truck = new Truck(3);
-   }
 
    @Test
     public void whenParkingLotForLightCarFreeThenTrue() {
+       LightCar lightCar = new LightCar();
        ParkingLot parkingLot = new ParkingLot(new Car[1]);
        assertTrue(lightCar.move(lightCar, parkingLot ));
    }
 
    @Test
    public void whenParkingLotForLightCarTakenThenFalse() {
+      LightCar lightCar = new LightCar();
+      Truck truck = new Truck(3);
       ParkingLot parkingLot = new ParkingLot(new Car[] {truck, truck , truck});
       assertFalse(lightCar.move(lightCar, parkingLot));
    }
 
    @Test
    public void whenParkingLotNumberOneForTruckTakenThenFalse() {
+      LightCar lightCar = new LightCar();
+      Truck truck = new Truck(3);
       ParkingLot parkingLot = new ParkingLot(new Car[] {lightCar, null, null});
       assertFalse(lightCar.move(truck, parkingLot));
    }
 
    @Test
    public void whenParkingLotNumberTwoForTruckTakenThenFalse() {
+      LightCar lightCar = new LightCar();
+      Truck truck = new Truck(3);
       ParkingLot parkingLot = new ParkingLot(new Car[] {null, lightCar, null});
       assertFalse(lightCar.move(truck, parkingLot));
    }
 
    @Test
    public void whenParkingLotNumberThreeForTruckTakenThenFalse() {
+      LightCar lightCar = new LightCar();
+      Truck truck = new Truck(3);
       ParkingLot parkingLot = new ParkingLot(new Car[] {null, null, lightCar});
       assertFalse(lightCar.move(truck, parkingLot));
    }
 
    @Test
    public void whenParkingLotForTruckFreeThenTrue() {
+      Truck truck = new Truck(3);
       ParkingLot parkingLot = new ParkingLot(new Car[3]);
       assertTrue(truck.move(truck, parkingLot ));
    }
 
    @Test
    public void whenParkingLotForLightCarAtFirstThenTrue() {
+      LightCar lightCar = new LightCar();
+      Truck truck = new Truck(3);
       ParkingLot parkingLot = new ParkingLot(new Car[] {null, truck, truck, truck});
       assertTrue(lightCar.move(lightCar, parkingLot));
    }
 
    @Test
    public void whenParkingForBothCarsFree() {
+      LightCar lightCar = new LightCar();
+      Truck truck = new Truck(3);
       ParkingLot parkingLot = new ParkingLot(new Car[4]);
       assertTrue(lightCar.move(lightCar, parkingLot));
-      assertTrue(lightCar.move(truck, parkingLot));
+      assertTrue(truck.move(truck, parkingLot));
    }
 
    @Test
-   public void whenParkingLotForLightCarAtTheEndFirstThenTrue() {
+   public void whenParkingLotForLightCarAtTheEndThenTrue() {
+      LightCar lightCar = new LightCar();
+      Truck truck = new Truck(3);
       ParkingLot parkingLot = new ParkingLot(new Car[] {truck, truck, truck, null});
       assertTrue(lightCar.move(lightCar, parkingLot));
    }
